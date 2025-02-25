a3f82bafbeb328d597b92f410afec3b8e9861d6e
Sigalaeq <13039@portalcedi.edu.mx>
2025-02-25 10:42:03 -0600

    pro

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonItem, IonLabel, IonInput, IonCard, IonCardHeader, IonCardContent, IonCardTitle } from '@ionic/angular/standalone';
+import { OpenaiService } from '../openai.service';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonItem, IonLabel, IonInput, IonCard, IonCardHeader, IonCardContent, IonCardTitle, FormsModule],
 })
 export class HomePage {
-  constructor() {}
+  constructor(private router: Router, private OpenAIService: OpenaiService) { }
+  ideaPrompt: string = '';
+  generatedIdea: string = '';
+
+
+  async generateIdea() {
+    if (this.ideaPrompt.trim() === '') {
+      alert('Por favor, ingresa una idea');
+      return;
+    }
+    this.generatedIdea = await this.OpenAIService.generateIdea(this.ideaPrompt);
+  }
 }
